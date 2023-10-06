9bec5c03b0782bc00c32946feaee4d5226b60b84
mendejumrukovski <mende@code.livelike.com>
2023-10-07 00:22:31 +0200

    refactor from ktlint

--- app/src/main/java/dev/mende273/foody/MainActivity.kt
+++ app/src/main/java/dev/mende273/foody/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dev.mende273.foody.ui.theme.FoodyTheme
 
 class MainActivity : ComponentActivity() {
