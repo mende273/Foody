79afed3839995ef900dc1f5b4828b4f68194eb87
mendejumrukovski <mende@code.livelike.com>
2023-10-10 00:36:46 +0200

    implement navigation

---
+++ app/src/main/java/dev/mende273/foody/ui/screen/main/MainActivity.kt
@@ -0,0 +1,101 @@
+package dev.mende273.foody.ui.screen.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import dev.mende273.foody.navigation.AppNavigation
+import dev.mende273.foody.navigation.Screen
+import dev.mende273.foody.ui.theme.FoodyTheme
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3WindowSizeClassApi::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            val navController = rememberNavController()
+            val windowSize = calculateWindowSizeClass(this)
+
+            var currentMenuItemIndex by rememberSaveable { mutableIntStateOf(0) }
+
+            val isPortrait = windowSize.widthSizeClass <= WindowWidthSizeClass.Medium
+
+            val navigationMenuItems = enumValues<NavigationMenuItem>()
+
+            FoodyTheme {
+                Scaffold(
+                    content = { innerPadding ->
+                        AppNavigation(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(MaterialTheme.colorScheme.background),
+                            navHostController = navController,
+                            innerPadding = innerPadding
+                        )
+                    },
+                    bottomBar = {
+                        if (isPortrait) {
+                            PortraitNavigationBar(
+                                navigationMenuItems = navigationMenuItems,
+                                currentMenuItem = currentMenuItemIndex,
+                                onMenuItemClick = { index, menuItem ->
+                                    currentMenuItemIndex = index
+                                    navController.navigateFromMenuItem(menuItem)
+                                }
+                            )
+                        }
+                    }
+                )
+
+                if (!isPortrait) {
+                    LandscapeNavigationBar(
+                        navigationMenuItems = navigationMenuItems,
+                        currentMenuItem = currentMenuItemIndex,
+                        onMenuItemClick = { index, menuItem ->
+                            currentMenuItemIndex = index
+                            navController.navigateFromMenuItem(menuItem)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+private fun NavHostController.navigateFromMenuItem(
+    menuItem: NavigationMenuItem
+) {
+    val route = when (menuItem) {
+        NavigationMenuItem.MEALS -> Screen.Meals.route
+        NavigationMenuItem.RANDOM_MEAL -> Screen.RandomMeal.route
+        NavigationMenuItem.SEARCH -> Screen.Search.route
+        NavigationMenuItem.FAVORITES -> Screen.Favourites.route
+    }
+
+    navigate(route) {
+        popUpTo(graph.findStartDestination().id) {
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
+}
--- app/src/main/java/dev/mende273/foody/ui/screen/meals/MealsScreen.kt
+++ app/src/main/java/dev/mende273/foody/ui/screen/meals/MealsScreen.kt
@@ -1,6 +1,7 @@
 package dev.mende273.foody.ui.screen.meals
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -8,7 +9,7 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun MealsScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(text = "Meals Screen")
     }
 }
