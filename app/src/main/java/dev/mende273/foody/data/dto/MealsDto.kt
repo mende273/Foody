4c3f4b64c30642406b8584cdcc0df34a47ba107a
mendejumrukovski <mende@code.livelike.com>
2023-10-13 14:31:50 +0200

    create meal dto

---
+++ app/src/main/java/dev/mende273/foody/data/dto/MealsDto.kt
@@ -0,0 +1,10 @@
+package dev.mende273.foody.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+class MealsDto(
+    @SerialName("meals")
+    val meals: List<MealDto>
+)
