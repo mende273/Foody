96a38f99ed73e7b8b10d06eb4727074970120dd3
mendejumrukovski <mende@code.livelike.com>
2023-10-15 23:24:15 +0200

    create ProgressBar

---
+++ app/src/main/java/dev/mende273/foody/ui/component/ProgressBar.kt
@@ -0,0 +1,25 @@
+package dev.mende273.foody.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.mende273.foody.ui.theme.AccentColor
+
+@Composable
+@Preview
+fun ProgressBar(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.background(MaterialTheme.colorScheme.background),
+            color = AccentColor
+        )
+    }
+}
