176e68542c09997c349c95c0c1cf46f7a0b69584
mendejumrukovski <mende@code.livelike.com>
2023-11-03 23:50:37 +0100

    refactor package structure

--- app/src/main/java/mende273/foody/domain/usecase/GetMealsForAreaUseCase.kt
+++ app/src/main/java/mende273/foody/domain/usecase/GetMealsForAreaUseCase.kt
@@ -1,8 +1,8 @@
 package mende273.foody.domain.usecase
 
 import mende273.foody.data.repository.RemoteRepositoryImpl
-import mende273.foody.domain.model.Meals
 import mende273.foody.domain.mapper.toDomain
+import mende273.foody.domain.model.Meals
 import mende273.foody.util.exception.RetrieveNetworkDataException
 
 class GetMealsForAreaUseCase(private val remoteRepository: RemoteRepositoryImpl) {
