0738938078bb98f80a0cdbe0f163733a19d066ee
mendejumrukovski <mende@code.livelike.com>
2023-11-06 00:58:22 +0100

    implement sqlDelight

---
+++ app/src/main/java/mende273/foody/data/dao/MealDao.kt
@@ -0,0 +1,25 @@
+package mende273.foody.data.dao
+
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToOneOrNull
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import mende273.foody.Database
+import mende273.foody.MealEntity
+
+class MealDao(private val database: Database) {
+    private val query get() = database.mealQueries
+
+    fun selectAll(): List<MealEntity> = query.selectAll().executeAsList()
+
+    fun insert(mealEntity: MealEntity) {
+        query.insert(mealEntity)
+    }
+
+    fun selectById(id: Long): Flow<MealEntity?> =
+        query.selectById(id).asFlow().mapToOneOrNull(Dispatchers.IO)
+
+    fun delete(id: Long) {
+        query.delete(id)
+    }
+}
---
+++ app/src/main/java/mende273/foody/di/DatabaseModule.kt
@@ -0,0 +1,23 @@
+package mende273.foody.di
+
+import android.content.Context
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import mende273.foody.Database
+import mende273.foody.data.dao.MealDao
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val databaseModule = module {
+    factory { sqlDriverFactory(androidContext()) }
+    single { createDatabase(driver = get()) }
+    single { MealDao(database = get()) }
+}
+
+fun sqlDriverFactory(context: Context): SqlDriver {
+    return AndroidSqliteDriver(Database.Schema, context, "foody.db")
+}
+
+fun createDatabase(driver: SqlDriver): Database {
+    return Database(driver)
+}
