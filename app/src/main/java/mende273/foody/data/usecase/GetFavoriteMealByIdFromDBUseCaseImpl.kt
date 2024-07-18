ba0609e3b2759d8545fcd5a7c674cdbb351b3ac8
mende273 <mende_273@hotmail.com>
2024-07-18 15:27:30 +0200

    create GetFavoriteMealByIdFromDBUseCase

---
+++ app/src/main/java/mende273/foody/data/usecase/GetFavoriteMealByIdFromDBUseCaseImpl.kt
@@ -0,0 +1,14 @@
+package mende273.foody.data.usecase
+
+import kotlinx.coroutines.flow.Flow
+import mende273.foody.domain.model.Meal
+import mende273.foody.domain.repository.LocalRepository
+import mende273.foody.domain.usecase.GetFavoriteMealByIdFromDBUseCase
+
+class GetFavoriteMealByIdFromDBUseCaseImpl
+(private val localRepository: LocalRepository) : GetFavoriteMealByIdFromDBUseCase {
+
+    override suspend fun invoke(id: Long): Flow<Meal?> {
+        return localRepository.getFavouriteMealById(id)
+    }
+}
