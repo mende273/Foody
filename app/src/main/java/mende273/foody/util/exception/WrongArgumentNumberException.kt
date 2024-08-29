8b378dd1f4f68d51f451b3668749f358a3ea9bf5
mende273 <mende_273@hotmail.com>
2024-08-30 01:16:18 +0200

    remove unused WrongArgumentNumberException.kt

--- app/src/main/java/mende273/foody/util/exception/WrongArgumentNumberException.kt
+++
@@ -1,7 +0,0 @@
-package mende273.foody.util.exception
-
-class WrongArgumentNumberException :
-    Exception(
-        "Provided arguments number is greater " +
-            "or lower than the specified navArgument list"
-    )
