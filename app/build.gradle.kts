d0469482877e0bf588def917c3f1ca1a5959833e
mende273 <mende_273@hotmail.com>
2024-09-10 15:16:24 +0200

    version bump to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "mende273.foody"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
